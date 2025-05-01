b0c6dd5827808c5c4a4e3a36249869ff52938391
aespi <aespi125@ucr.edu>
2025-05-01 13:29:42 -0700

    added depth arg

--- reddit_crawler/crawler.py
+++ reddit_crawler/crawler.py
@@ -3,11 +3,17 @@ from reddit_crawler.request import fetch   # Include "fetch" function from reque
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("usage: python -m reddit_crawler.crawler <filename> [dephtLimit]")
+        print("usage: python -m reddit_crawler.crawler <filename> [limit=1000]")
         sys.exit(1)
 
     try:
         filename = sys.argv[1]
+
+        try:
+            limit = sys.argv[2] if len(sys.argv) > 2 else 1000
+        except ValueError:
+            print("Argument must be an integer")
+            sys.exit(1)
 
         for url in open(filename, "r"):
             response = fetch(url) # Send request
